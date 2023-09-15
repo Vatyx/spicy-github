d250df6afdc6a5f8a5d438a8547cfc32187a4591
wallstop <dbfsinfil@gmail.com>
2023-09-15 16:19:00 -0700

    Initial floundering on db insertion

---
+++ src/spicy_github/changeset.clj
@@ -0,0 +1,2 @@
+(ns spicy-github.changeset)
+
--- test/spicy_github/db_test.clj
+++ test/spicy_github/db_test.clj
@@ -1,6 +1,18 @@
 (ns spicy-github.db-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clojure.data.json :as json]
+            [gungnir.query :as query]
+            [gungnir.changeset :as changeset]))
 
-(deftest insert-into-db)
+(defn parse-json [path]
+  (json/read-str (slurp path)))
 
-(deftest remove-from-db)
+; https://stackoverflow.com/questions/15660066/how-to-read-json-file-into-clojure-defrecord-to-be-searched-later
+(defn cast-comment [comment]
+  (changeset/cast :comment comment))
+
+(defn cast-user [comment]
+  (changeset/cast :user (:user comment)))
+
+(defn seed-db []
+  (run! (juxt cast-comment cast-user) (parse-json "test/test_data/comments-16270.json")))
