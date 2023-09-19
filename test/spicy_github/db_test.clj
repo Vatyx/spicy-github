35068bdf4d6122afb92397dbfc9512e99fbaceaa
wallstop <dbfsinfil@gmail.com>
2023-09-18 21:04:13 -0700

    Fix everything

--- test/spicy_github/db_test.clj
+++ test/spicy_github/db_test.clj
@@ -33,56 +33,59 @@
   (let [comment (comment-and-parent 0)
         parent (comment-and-parent 1)
         issue-id (comment-and-parent 2)]
-    (let [updated-comment (assoc comment "id" (str ("get" comment "id"))
-                                         "github-json-payload" (json/write-str comment)
-                                         "comment-creation-time" (parse-timestamp (get comment "created_at"))
-                                         "comment-updated-time" (parse-timestamp (get comment "updated_at"))
-                                         "user-id" (str (get-in comment ["user" "id"]))
-                                         "issue-id" issue-id)]
+    (let [updated-comment {:comment/id (-> comment :id str)
+                           :comment/url (:url comment)
+                           :comment/body (:body comment)
+                           :comment/comment-creation-time (:created_at comment)
+                           :comment/comment-updated-time (:updated_at comment)
+                           :comment/issue-id issue-id
+                           :comment/user-id (-> comment :user :id str)
+                           :comment/github-json-payload (json/write-str comment)
+                           }]
       (if
         (nil? parent)
         updated-comment
-        (assoc updated-comment "parent-comment" (str (get parent "id")))))))
+        (conj updated-comment {:comment/parent-comment (-> parent :id str)})))))
 
 (defn parse-user [user-json]
-  (assoc user-json "id" (str (get user-json "id"))
-                   "url" (get user-json "html_url")
-                   "avatar-url" (get user-json "avatar_url")))
+  {:user/id (-> user-json :id str)
+   :user/avatar-url (:avatar_url user-json)
+   :user/url (:url user-json)
+   })
 
-(defn parse-issue [issue-json comment]
-  (dissoc
-    (let [updated-issue (assoc issue-json "id" (str (get issue-json "id"))
-                                          "total-reactions" (get (get issue-json "reactions") "total_count")
-                                          "comment-count" (get issue-json "comments")
-                                          "issue-creation-date" (get issue-json "created_at")
-                                          "issue-updated-time" (get issue-json "updated_at")
-                                          "github-json-payload" (json/write-str issue-json))]
-      (if
-        (nil? comment)
-        updated-issue
-        (assoc updated-issue "root-comment" (str (get comment "id")))))
-    "comments"))
+(defn parse-issue [issue-json]
+  {:issue/id (-> issue-json :id str)
+   :issue/url (:url issue-json)
+   :issue/title (:title issue-json)
+   :issue/body (:body issue-json)
+   :issue/total-reactions (-> issue-json :reactions :total_count int)
+   :issue/comment-count (-> issue-json :comments int)
+   :issue/issue-creation-time (:created_at issue-json)
+   :issue/issue-updated-time (:updated_at issue-json)
+   :issue/user-id (-> issue-json :user :id str)
+   :issue/github-json-payload (json/write-str issue-json)
+   })
 
-(defn parse-user-from-comment [comment] (parse-user (get comment "user")))
-(defn parse-user-from-issue [issue] (parse-user (get issue "user")))
+(defn parse-user-from-comment [comment] (-> comment :user parse-user))
+(defn parse-user-from-issue [issue] (-> issue :user parse-user))
 
 (defn create-or-update-comment! [comment-and-parent]
   (-> comment-and-parent
       parse-comment
       changeset/create
       (persist!
-        (fn [record] (query/find! :comment (get record :comment/id)))
+        (fn [record] (query/find! :comment (:comment/id record )))
         (fn [existing record] (changeset/create existing (changeset/cast record :comment)))
         (fn [lhs rhs]
           (=
-            (get lhs :comment/body)
-            (get rhs :comment/body))))))
+            (if (nil? lhs) 0 (inst-ms (:comment/comment-updated-time lhs)))
+            (if (nil? rhs) 0 (inst-ms (:comment/comment-updated-time rhs))))))))
 
 (defn create-or-update-user! [user]
   (-> user
       changeset/create
       (persist!
-        (fn [record] (query/find! :user (get record :user/id)))
+        (fn [record] (query/find! :user (:user/id record)))
         (fn [existing record] (changeset/create existing (changeset/cast record :user)))
         (fn [lhs rhs]
           (=
@@ -90,21 +93,17 @@
             (get rhs :user/avatar-url)))))
   )
 
-(defn create-or-update-issue! [issue comment]
+(defn create-or-update-issue! [issue]
   (-> issue
-      (parse-issue comment)
+      parse-issue
       changeset/create
       (persist!
-        (fn [record] (query/find! :issue (get record :issue/id)))
+        (fn [record] (query/find! :issue (:issue/id record)))
         (fn [existing record] (changeset/create existing (changeset/cast record :issue)))
         (fn [lhs rhs]
-          (and (=
-                 (inst-ms (:issue/issue-updated-time lhs))
-                 (inst-ms (:issue/issue-updated-time rhs)))
-               (=
-                 (:issue/root-comment lhs)
-                 (:issue/root-comment rhs))
-               )))
+          (=
+            (if (nil? lhs) 0 (inst-ms (:issue/issue-updated-time lhs)))
+            (if (nil? rhs) 0 (inst-ms (:issue/issue-updated-time rhs))))))
       ))
 
 (defn create-or-update-user-from-comment! [comment]
@@ -122,17 +121,15 @@
 (defn seed-db! []
   (let [issue (parse-json "test/test_data/issue-16270.json")
         comments (parse-json "test/test_data/comments-16270.json")
-        issue-id (str (get issue "id"))]
+        issue-id (-> issue :id str)]
     ; Create root user
     (create-or-update-user-from-issue! issue)
     ; Then the issue
-    (create-or-update-issue! issue nil)
+    (create-or-update-issue! issue)
     ; Then all users for the comments
     (run! create-or-update-user-from-comment! comments)
     ; Then the comments themselves
     (run! create-or-update-comment! (map vector comments (drop-last (conj (seq comments) nil)) (repeat issue-id)))
-    ; And finally, link our issue back to the root comment
-    (create-or-update-issue! issue (nth comments 0 (fn [] nil)))
     ))
 
 (defn setup-test-env! []
