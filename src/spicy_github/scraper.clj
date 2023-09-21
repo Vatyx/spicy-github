26353ef373b82460484e839ae1ef37bf9e6ebe14
Sahil Dhanju <sahildhanju1@gmail.com>
2023-09-21 02:09:59 +0200

    Cleanup

--- src/spicy_github/scraper.clj
+++ src/spicy_github/scraper.clj
@@ -1,15 +1,33 @@
 (ns spicy-github.scraper
   (:gen-class)
-  (:require [spicy-github.db :as db]
-            [spicy-github.model :as model]
-            [spicy-github.adapters :as adapters]
-            [spicy-github.util :refer :all]
-            [malli.dev.pretty]
-            [clj-http.client :as client]
-            [cheshire.core :refer :all]
-            [gungnir.model]
-            [gungnir.query :as q]
-            [gungnir.changeset :as changeset]))
+    (:require [clojure.java.io :as io]
+              [spicy-github.db :as db]
+              [spicy-github.model :as model]
+              [spicy-github.adapters :as adapters]
+              [spicy-github.util :refer :all]
+              [malli.dev.pretty]
+              [clj-http.client :as client]
+              [cheshire.core :refer :all]
+              [clojure.edn :as edn]
+              [gungnir.model]
+              [gungnir.query :as q]
+              [gungnir.changeset :as changeset]
+              [throttler.core :refer [throttle-fn]]))
+
+(defn get-github-token []
+    (-> (io/resource "token.edn")
+        io/file
+        slurp
+        edn/read-string
+        :github-token))
+(def get-url (throttle-fn client/get 5000 :hour))
+
+(def github-token (get-github-token))
+
+(defn get-github-url [url]
+    (get-url url {:headers {"Authorization" (str "Bearer " github-token)}}))
+
+(get-github-url "https://api.github.com/repos/dakrone/cheshire")
 
 (defn process-repositories []
     (forever
@@ -18,68 +36,3 @@
 
 (defn get-repositories []
     (q/all! :repository))
-
-(q/all! :repository)
-
-(comment
-
-(def get-url (throttle-fn client/get 50 :hour))
-
-(defn get-and-parse [url]
-  (parse-string (:body (client/get url))))
-
-(db/initialize-db!)
-(model/register-models!)
-
-(q/save! (changeset/create {:repository/url "what" :repository/processed true}))
-
-(comment
-  (def client (github-client/new-client {:app-id "spicy-github" :private-key "SHA256:qc+fDGaDm6F8/j+IiKFr/4wef9qnhUKD/g7AW8avih4=\n"}))
-
-  (github-client/request client {:url "https://api.github.com/repositories/211666/contributors" :method :get})
-
-  (require '[clj-http.client :as client])
-
-  (def foo (client/get "https://api.github.com/repos/dakrone/cheshire"))
-
-  (:body foo)
-
-  (def repo (parse-string (:body foo), true))
-
-  (adapters/parse-repository repo)
-
-  (db/persist-record! (adapters/parse-repository repo))
-
-  (get-and-parse "https://api.github.com/repos/ziglang/zig/issues/16270")
-
-  (def issues (get-and-parse "https://api.github.com/repos/ziglang/zig/issues/16270"))
-
-  (get issues "comments")
-
-  (def comments (get-and-parse "https://api.github.com/repos/ziglang/zig/issues/16270/comments"))
-
-  comments
-
-  (def specific-issues (get-and-parse "https://api.github.com/repos/ziglang/zig/issues/17065/reactions"))
-
-  (get-and-parse "https://api.github.com/repos/ziglang/zig/issues/17065/reactions")
-
-  (db/initialize-db!)
-
-  (-> {"url" "test" "processed" false}
-      (gungnir.changeset/cast :repository)
-      (gungnir.changeset/create)
-      (q/save!))
-
-  (def huh (random-uuid))
-  (changeset/create {:repository/url "test"})
-
-  (q/save! (changeset/create {:repository/url "test" :repository/processed false}))
-
-  (try
-
-     (catch Exception e
-       (malli.dev.pretty/explain model/repository-model e)
-       ))
-)
-)
