cccff1b901b7d7ddfee4f8971f41f535d289e9ad
wallstop <dbfsinfil@gmail.com>
2023-09-23 20:23:48 -0700

    Enhance pointers

--- src/spicy_github/frontend.clj
+++ src/spicy_github/frontend.clj
@@ -22,24 +22,60 @@
       [:style {:id "_stylefy-server-styles_"} "_stylefy-server-styles-content_"] ; Generated CSS will be inserted here
       [:style {:id "_stylefy-constant-styles_"}]
       [:style {:id "_stylefy-styles_"}]
-      [:style "a {text-decoration: none; color: #5c55fc; font-weight: bold}"]]
+      [:style "a {text-decoration: none; color: #5c55fc; font-weight: bold}"]
+      [:style "details summary::marker {display:none;} summary{list-style: none} details {}"]
+      ; TODO: Copy this script to our codebase so we're not hotlinking
+      [:script {:type "module" :src "https://md-block.verou.me/md-block.js"}]]
      [:body body]])
 
-(defn comment-style [] {:border-radius :10px :margin :10px :color :#fff :display :flex})
 
-(defn comment-container-style [] {:display :flex :background-color :#333 :color :#fff :padding "5px 15px 5px 15px" :border-radius :20px :margin :10px :opacity :0.8})
+(defn comment-style [] {:border-radius :10px
+                        :margin        :10px
+                        :color         :#fff
+                        :display       :flex})
 
-(defn comment-body-style [] {:flex :9 :display :inline :padding "5px 20px 5px 20px"})
+(defn comment-container-style [] {:display          :flex
+                                  :background-color :#333
+                                  :color            :#fff
+                                  :padding          "5px 15px 5px 15px"
+                                  :border-radius    :20px
+                                  :margin           :10px
+                                  :opacity          :0.8
+                                  :cursor           :auto})
 
-(defn issue-style [] {:border-radius :20px :margin-bottom :20px :display :flex :box-sizing :border-box :background-color :#ccc :color :#333 :flex-direction :column})
+(defn comment-body-style [] {:flex    :9
+                             :display :inline
+                             :padding "5px 20px 5px 20px"})
 
-(defn issue-body-style [] {:flex :9 :padding "5px 5px 20px 20px"})
+(defn issue-style [] {:border-radius  :20px
+                      :margin-bottom  :20px
+                      :display        :flex :box-sizing
+                      :border-box :background-color
+                      :#ccc :color    :#333
+                      :flex-direction :column})
 
-(defn issue-container-style [] {:display :flex})
+(defn issue-body-style [] {:flex    :9
+                           :padding "5px 5px 20px 20px"
+                           :cursor :pointer})
 
-(defn issue-user-image-style [] {:background-color :#fff :border-radius :50% :width :100px :height :100px :flex "0 0 100px" :padding :10px :margin :10px})
+(defn issue-container-style [] {
+                                :display :flex})
 
-(defn user-image-style [] {:background-color :#fff :border-radius :50% :width :100px :height :100px :flex "0 0 100px" :padding :10px :margin-top :10px :margin-bottom :10px})
+(defn issue-user-image-style [] {:background-color :#fff
+                                 :border-radius    :50%
+                                 :width            :100px :height
+                                 :100px :flex      "0 0 100px"
+                                 :padding          :10px
+                                 :margin           :10px})
+
+(defn user-image-style [] {:background-color :#fff
+                           :border-radius    :50%
+                           :width            :100px
+                           :height           :100px
+                           :flex             "0 0 100px"
+                           :padding          :10px
+                           :margin-top       :10px
+                           :margin-bottom    :10px})
 
 (defn get-user-html
     ([user] (get-user-html user user-image-style))
@@ -50,8 +86,8 @@
     [:div (stylefy/use-style (comment-style))
      (-> comment :comment/user get-user-html)
      [:div (stylefy/use-style (comment-container-style))
-      [:div (merge (stylefy/use-style (comment-body-style)))
-      (-> (:comment/body comment) nextjournal.markdown/->hiccup)]]])
+      [:div (stylefy/use-style (comment-body-style))
+       [:md-block (:comment/body comment)]]]])
 
 (defn get-ordered-comments [comments]
     (let [ordered-by-date-comments (sort-by :comment/updated-at comments)
@@ -76,10 +112,13 @@
 
 (defn get-issue-html [issue]
     [:div (stylefy/use-style (issue-style))
-     [:div (stylefy/use-style (issue-container-style))
-      [:div (stylefy/use-style (issue-body-style)) (-> (:issue/body issue) nextjournal.markdown/->hiccup)]
-      (-> (:issue/user issue) (get-user-html issue-user-image-style))]
-     (vec (conj (seq (->> (:issue/comments issue) get-ordered-comments (map get-comment-html))) :div))])
+     [:details
+      [:summary [:div (stylefy/use-style (issue-container-style))
+                 [:md-block (stylefy/use-style (issue-body-style)) (:issue/body issue)]
+                 (-> (:issue/user issue) (get-user-html issue-user-image-style))]]
+      (vec (conj (seq (->> (:issue/comments issue) get-ordered-comments (map get-comment-html))) :div))
+      ]
+     ])
 
 (defn get-issues-html [issues]
     [:div (vec (conj (seq (map get-issue-html issues)) :div))])
