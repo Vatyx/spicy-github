d181556e3237b2f00bda76634e5369463a5914eb
wallstop <dbfsinfil@gmail.com>
2023-10-21 15:01:15 -0700

    Fix front end

--- front_end/src/spicy_github/api.cljs
+++ front_end/src/spicy_github/api.cljs
@@ -10,16 +10,17 @@
     ([] (get-n-issues-before-endpoint (.now js/Date)))
     ([before] (str spicy-endpoint "latest-issues/" (.toISOString (js/Date. before)))))
 
-(defn- get-n-issues-before-get-response [endpoint response-fn]
+(defn- get-n-issues-before-get-response [endpoint response-fn is-loading-issues]
     (go (let [response (<! (http/get endpoint {}))]
+            (reset! is-loading-issues false)
             (when (== 200 (:status response))
                 (response-fn (js->clj (:body response) :keywordize-keys true))))))
 
 (defn get-n-issues-before
-    ([response-fn] (get-n-issues-before-get-response (get-n-issues-before-endpoint) response-fn))
-    ([response-fn before] (get-n-issues-before-get-response (get-n-issues-before-endpoint before) response-fn)))
+    ([response-fn is-loading-issues] (get-n-issues-before-get-response (get-n-issues-before-endpoint) response-fn is-loading-issues))
+    ([response-fn is-loading-issues before] (get-n-issues-before-get-response (get-n-issues-before-endpoint before) response-fn is-loading-issues)))
 
-(defn get-n-issues-before-from-issues [response-fn issues]
+(defn get-n-issues-before-from-issues [response-fn issues is-loading-issues]
     (if (empty? issues)
-        (get-n-issues-before response-fn)
-        (get-n-issues-before response-fn (js/Date. (:issue/updated-at (last issues))))))
+        (get-n-issues-before response-fn is-loading-issues)
+        (get-n-issues-before response-fn is-loading-issues (js/Date. (:issue/updated-at (last issues))))))
