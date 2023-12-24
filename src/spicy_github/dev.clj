33cb44cd1cf968745757d9788452a6c852484573
Eli Pinkerton <dbfsinfil@gmail.com>
2023-12-24 12:01:14 -0800

    Dev/wallstop/fix remap checkpointing (#41)
    * Fix remap checkpointing
    
    * Remove date import

--- src/spicy_github/dev.clj
+++ src/spicy_github/dev.clj
@@ -21,8 +21,9 @@
           checkpoint (db/get-by-id! :checkpoint checkpoint-id)
           checkpoint-time (spicy-github.adapters/checkpoint-get-time checkpoint (Instant/now))]
         (loop [current-time checkpoint-time]
-            (let [issue-batch (db-query! record-count current-time)]
-                (db/persist-record! (spicy-github.adapters/checkpoint-create checkpoint-id current-time))
+            (let [issue-batch (db-query! record-count current-time)
+                  current-checkpoint (spicy-github.adapters/checkpoint-create checkpoint-id current-time)]
+                (db/persist-record! current-checkpoint)
                 (run!
                     #(db/persist-record! (parse-fn (parse-json (json-payload-keyword %))))
                     issue-batch)
@@ -30,7 +31,9 @@
                 (Thread/sleep (int (rand 5000)))
                 (if (<= record-count (count issue-batch))
                     (recur (updated-at-keyword (first (sort-by updated-at-keyword issue-batch))))
-                    (timbre/info (str "Successfully remapped " table-name)))))))
+                    (do
+                        (db/delete! current-checkpoint)
+                        (timbre/info (str "Successfully remapped " table-name))))))))
 
 (defn remap-issues! []
     (remap! db/get-n-latest-issues-before! adapters/parse-issue :issue/github-json-payload :issue/updated-at "issues"))
