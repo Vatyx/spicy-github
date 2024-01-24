b7d35dfd6e9d74cb88cff44a052f7f4e12089783
wallstop <wallstop@wallstopstudios.com>
2024-01-23 16:38:48 -0800

    Update publish script

--- publish.ps1
+++ publish.ps1
@@ -2,8 +2,8 @@ lein clean
 lein uberjar
 $version = "1.0-" + (Get-Date -format "yyyy-MM-dd_HH-mm-ss")
 $filename = "spicy-github_" + $version + ".jar"
-$s3key = "app/" + $filename
-$s3location = "s3://spicy-github/" + $s3key
+$s3key = $filename
+$s3location = "s3://elasticbeanstalk-us-west-2-445413863088/" + $s3key
 aws s3 cp ./target/uberjar/spicy-github-0.1.0-SNAPSHOT-standalone.jar $s3location
-aws elasticbeanstalk create-application-version --application-name SpicyGithubv3 --version-label $version --source-bundle S3Bucket="spicy-github",S3Key=$s3key
+aws elasticbeanstalk create-application-version --application-name SpicyGithubv3 --version-label $version --source-bundle S3Bucket="elasticbeanstalk-us-west-2-445413863088",S3Key=$s3key
 aws elasticbeanstalk update-environment --application-name SpicyGithubv3 --environment-name SpicyGithubv3-env --version-label $version
