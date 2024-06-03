d0e596dc100525e61701299af27c07bdfefa7155
wallstop <wallstop@wallstopstudios.com>
2024-06-03 14:21:31 -0700

    Update gitignore

---
+++ profiles.clj
@@ -0,0 +1,6 @@
+{:dev     {:env {:spicy-endpoint "http://localhost"
+                 :front-end-port "5000"}}
+ :uberjar {:env {:spicy-endpoint "https://gitfeed.app"
+                 :front-end-port "80"}}
+ :jar     {:env {:spicy-endpoint "https://gitfeed.app"
+                 :front-end-port "80"}}}
