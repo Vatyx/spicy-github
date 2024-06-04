2615996ee42fdef39a7414e3d38276971b347e83
Sahil Dhanju <sahildhanju1@gmail.com>
2024-06-04 02:19:18 +0200

    profile changes

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,11 @@
-{:dev     {:env {:spicy-endpoint "http://localhost"
-                 :front-end-port "5000"}}
+{:dev     {:env {:spicy-endpoint "http://localhost:3000/",
+                 :reload-server "true",
+                 :rds-hostname "localhost",
+                 :rds-port "5432",
+                 :rds-db-name "spicy-github",
+                 :rds-username "postgres",
+                 :rds-password "",
+                 :log-level "debug"}}
  :uberjar {:env {:spicy-endpoint "https://gitfeed.app"
                  :front-end-port "80"}}
  :jar     {:env {:spicy-endpoint "https://gitfeed.app"
